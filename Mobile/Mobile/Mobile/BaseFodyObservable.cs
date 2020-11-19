31ee1a72151448db77d0bc9c4c6649a40716c667
Charles-Erickson <charles.erickson414@gmail.com>
2020-11-18 22:04:26 -0600

    Added Card and Created base class

---
+++ Mobile/Mobile/Mobile/BaseFodyObservable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Mobile
+{
+    class BaseFodyObservable : INotifyPropertyChanged
+    {
+        #pragma warning disable CS0067
+        public event PropertyChangedEventHandler PropertyChanged;
+        #pragma warning restore
+
+    }
+}
---
+++ Mobile/Mobile/Mobile/Card.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mobile
+{
+    class Card : BaseFodyObservable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Element { get; set; }
+        public bool Evolution { get; set; }   
+    }
+}
