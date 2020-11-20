96e44c4d837b4eabad6736d093bc96f3e18cd98a
Charles-Erickson <charles.erickson414@gmail.com>
2020-11-19 20:37:46 -0600

    Grouped list

--- Mobile/Mobile/Mobile/DeckViewModel.cs
+++ Mobile/Mobile/Mobile/DeckViewModel.cs
@@ -1,19 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 
 namespace Mobile
 {
     class DeckViewModel : BaseFodyObservable
     {
+
+        public DeckViewModel()
+        {
+            GroupedTodoList = GetGroupedTodoList();
+        }
+        public ILookup<string, Card> GroupedTodoList { get; set; }
         public string Title => "Pokemon";
-        public new ObservableCollection<Card> Deck { get; set; } = new ObservableCollection<Card>
+        public new List<Card> _deck { get; set; } = new List<Card>
         {
             new Card { Id = 0, PokedexNum=1, Name = "Bulbasaur", Element="Grass", Evolution=true},
             new Card { Id = 1, PokedexNum=23, Name = "Bulbasaur", Element="Grass", Evolution=true},
             new Card { Id = 2, PokedexNum=144, Name = "Articuno", Element="Ice", Evolution=false}
         };
+        private ILookup<string, Card> GetGroupedTodoList()
+        {
+            return _deck.OrderBy(t => t.PokedexNum);
+        }
     }
 
 }
