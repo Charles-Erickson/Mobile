bd443587c54d1709b5fb25428c0119381509edaa
Charles-Erickson <charles.erickson414@gmail.com>
2020-11-23 19:15:29 -0600

    rollback group list

--- Mobile/Mobile/Mobile/DeckViewModel.cs
+++ Mobile/Mobile/Mobile/DeckViewModel.cs
@@ -9,22 +9,22 @@ namespace Mobile
     class DeckViewModel : BaseFodyObservable
     {
 
-        public DeckViewModel()
-        {
-            GroupedTodoList = GetGroupedTodoList();
-        }
+        //public DeckViewModel()
+        //{
+        //    GroupedTodoList = GetGroupedTodoList();
+        //}
         public ILookup<string, Card> GroupedTodoList { get; set; }
         public string Title => "Pokemon";
-        public new List<Card> _deck { get; set; } = new List<Card>
+        public new List<Card> Deck { get; set; } = new List<Card>
         {
             new Card { Id = 0, PokedexNum=1, Name = "Bulbasaur", Element="Grass", Evolution=true},
             new Card { Id = 1, PokedexNum=23, Name = "Bulbasaur", Element="Grass", Evolution=true},
             new Card { Id = 2, PokedexNum=144, Name = "Articuno", Element="Ice", Evolution=false}
         };
-        private ILookup<string, Card> GetGroupedTodoList()
-        {
-            return _deck.OrderBy(t => t.PokedexNum).ThenByDescending(t=>t.Element);
-        }
+        //private ILookup<string, Card> GetGroupedTodoList()
+        //{
+        //    return _deck.OrderBy(t => t.PokedexNum).ThenByDescending(t=>t.Element);
+        //}
     }
 
 }
